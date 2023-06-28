b56da41ca799da22149045de7120c650c22040f2
Shashank Chauhan <shashank.chauhan.ec.2018@miet.ac.in>
2023-06-28 13:16:35 +0530

    Add project files.

---
+++ EXE58/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EXE58
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            List<int> numbers = new List<int>() { 5, 1, 3, 7, 15, 100 };
+            int Max = numbers[0];
+            int Min = numbers[0];
+            foreach (int number in numbers)
+            {
+                if (number < Min)
+
+                    Min = number;
+
+                if (number > Max)
+
+                    Max = number;
+            }
+                
+            int count = 0;
+                for(int i = Min + 1; i < Max; i++)
+                {
+                    if (!numbers.Contains(i))
+                    
+                        count++;
+                    
+
+                }
+                Console.WriteLine(count);
+                Console.ReadLine();
+
+            
+        }
+    }
+}
